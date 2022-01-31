6210316a48cc194f3b1bd5bfa0b49313d3b91ecd
kingrayhan <rayhan095@gmail.com>
2022-02-01 04:32:32 +0600

    test:sendMessage()

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,22 +1,35 @@
-// const asset = require("assert");
-const { bytecode, abi } = require("../compile");
+const assert = require("assert");
+const compile = require("../compile");
 const Web3 = require("web3");
+const web3 = new Web3("http://127.0.0.1:7545");
 
-const web3 = new Web3(ganache.provider());
+let accounts;
+let inbox;
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  const { bytecode, abi } = compile("Inbox", "Inbox.sol");
 
-// let accounts;
-// let inbox;
-// beforeEach(async () => {
-//   accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(abi)
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
 
-//   inbox = await new web3.eth.Contract(JSON.parse(interface))
-//     .deploy({
-//       data: bytecode,
-//       arguments: ["Hi there!"],
-//     })
-//     .send({ from: accounts[0], gas: "1000000" });
-// });
+describe("Inbox", () => {
+  it("Inbox contract deployed", () => {
+    assert.ok(inbox.options.address);
+  });
 
-// it("deploying contract", () => {
-//   console.log(inbox);
-// });
+  it("has a initial message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hi there!");
+  });
+
+  it("can change message", async () => {
+    await inbox.methods.setMessage("New Message").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "New Message");
+  });
+});
